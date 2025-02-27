c0dad8679ecb670639782b85dbaef7b81e5c216a
Saurav Kumar Jha -IT-Business Enabler-Chola Thamarai – Guindy-Chola <sauravkumarjsk@chola.murugappa.com>
2025-02-27 18:02:41 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+//Create Express server 
+//install express
+const express = require('express');
+const app = express();
+
+//1
+// app.use("/",((req,res) =>{
+//     res.send("Namaste Saurav!")
+// }))
+// //2
+// app.use("/test",((req,res) =>{
+//     res.send("Hello from the server!")
+// }))
+// //3
+// app.use("/hello",((req,res) =>{
+//     res.send("Hello Hello Hello!")
+// }))
+//If we will change order 1 2 and 3 a lot of thing will change means code will execute from top to bottom approach
+//Routes  & how routes are working in order
+//Order of writing the route plays a key role role here
+
+
+app.get("/user",(req,res) =>{
+    res.send("Hello From the server");
+})
+
+
+//app.post
+//app.delete
+app.listen(2000,() => {
+    console.log("Server is successfully listening on port 2000");
+});
---
+++ src/text.md
@@ -0,0 +1,17 @@
+create a repository
+Initialise the repository
+Diff between node_modules,package.json, package-lock.json
+Install express
+Create a server
+Listen to port 7777
+Write request handlers for /test , /.hello
+Install nodemon
+diff bw caretr and tilda
+what is dependencies
+use of -g while npm install
+-create git repository and push all changes
+-Diff between package.json, package-lock.json
+-Routes  & how routes are working in order
+-Play with routes and routes extensions  e.g- ./hello, hello/2
+-Write logic to handle GET,PUT,PATCH,DELETE and test in postman
+
